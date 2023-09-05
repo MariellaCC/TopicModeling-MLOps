1da86b26f82dbdfc6d5ad9d92137ca26b8d12b27
JP Fuquen Paez <jpfuq93@gmail.com>
2023-09-05 11:20:21 +0200

    mlflow settings

--- model_training.py
+++ model_training.py
@@ -5,7 +5,8 @@ from gensim.models import LdaModel
 from gensim.test.utils import datapath
 from ast import literal_eval
 import os
-import mlflow 
+import mlflow
+
 
 """
 
@@ -24,104 +25,116 @@ By following this step-by-step execution, the code loads the corpus model data, 
 
 """
 
-
-
-def load_corpus_model(file_path):
-    """
-    Load the corpus model from a CSV file.
-
-    Args:
-        file_path (str): The path to the corpus model CSV file.
-
-    Returns:
-        pandas.Series: The corpus model data.
-
-    """
-    corpus_model = pd.read_csv(file_path)
-    bigrams = corpus_model['bigrams'].apply(literal_eval)
-    return bigrams
-
-
-def preprocess_corpus(bigrams):
-    """
-    Preprocess the corpus for LDA training.
-
-    Args:
-        bigrams (pandas.Series): The corpus data containing bigrams.
-
-    Returns:
-        gensim.corpora.Dictionary: The dictionary mapping of words to IDs.
-        list: The preprocessed corpus as a list of bag-of-words.
-
-    """
-    id2word = corpora.Dictionary(bigrams)
-    id2word.filter_extremes(no_below=5)
-    corpus = [id2word.doc2bow(text) for text in bigrams]
-    return id2word, corpus
-
-
-def train_lda_model(corpus, id2word, num_topics=7, random_state=100):
-    """
-    Train an LDA model on the given corpus.
-
-    Args:
-        corpus (list): The preprocessed corpus as a list of bag-of-words.
-        id2word (gensim.corpora.Dictionary): The dictionary mapping of words to IDs.
-        num_topics (int): The number of topics to generate (default: 7).
-        random_state (int): The random state for reproducibility (default: 100).
-
-    Returns:
-        gensim.models.LdaModel: The trained LDA model.
-
-    """
-    model = LdaModel(corpus, id2word=id2word, num_topics=num_topics, random_state=random_state, eval_every=None)
-    return model
-
-
-def save_model(model, output_path):
-    """
-    Save the trained LDA model to a file.
-
-    Args:
-        model (gensim.models.LdaModel): The trained LDA model.
-        output_path (str): The path to save the model.
-
-    """
-    model.save(output_path)
-
-
-if __name__ == "__main__":
-    
 # Set the default MLflow requirements for tracking
-    #MLflow Folder name definition
-    mlflow_folder_name = "MLflow"
-    #Get current directory to create MLflow directory 
-    current_directory = os.getcwd()
-    #full path constuction of the MLflow directory to be created
-    mlflow_path = os.path.join(current_directory, mlflow_folder_name, "mlruns")
+mlflow_folder_name = "MLflow"
+current_directory = os.getcwd()
+mlflow_path = os.path.join(current_directory, mlflow_folder_name, "mlruns")
 
-    #Check if directory exists else create it
-    if not os.path.exists(mlflow_path):
-        os.makedirs(mlflow_path)
-        print(f"Folder {mlflow_path} created successfully")
-    else: 
-        print(f"Folder {mlflow_path} already there")
+if not os.path.exists(mlflow_path):
+    os.makedirs(mlflow_path)
+    print(f"Folder {mlflow_path} created successfully")
+else:
+    print(f"Folder {mlflow_path} already exists")
 
-    #Set tracking URI
-    mlflow.set_tracking_uri("file://" + mlflow_path)
+mlflow.set_tracking_uri("file://" + mlflow_path)
+
+mlflow_experiment_name = "LDA_model_v1"
+experiment = mlflow.get_experiment_by_name(mlflow_experiment_name)
+
+if experiment is None:
+    mlflow_experiment_id = mlflow.create_experiment(mlflow_experiment_name)
+else:
+    mlflow_experiment_id = experiment.experiment_id
+
+with mlflow.start_run(experiment_id=mlflow_experiment_id):
     
-# Define the file paths
-    corpus_model_file = 'corpus_model.csv'
-    model_output_file = os.path.join(os.getcwd(), 'lda_model')
+    def load_corpus_model(file_path):
+        """
+        Load the corpus model from a CSV file.
 
-# Load the corpus model
-    corpus_model = load_corpus_model(corpus_model_file)
+        Args:
+            file_path (str): The path to the corpus model CSV file.
 
-# Preprocess the corpus
-    id2word, corpus = preprocess_corpus(corpus_model)
+        Returns:
+            pandas.Series: The corpus model data.
+        """
+        corpus_model = pd.read_csv(file_path)
+        bigrams = corpus_model['bigrams'].apply(literal_eval)
+        return bigrams
 
-# Train the LDA model
-    lda_model = train_lda_model(corpus, id2word)
 
-# Save the trained model to a file
-    save_model(lda_model, model_output_file)
+    def preprocess_corpus(bigrams):
+        """
+        Preprocess the corpus for LDA training.
+
+        Args:
+            bigrams (pandas.Series): The corpus data containing bigrams.
+
+        Returns:
+            gensim.corpora.Dictionary: The dictionary mapping of words to IDs.
+            list: The preprocessed corpus as a list of bag-of-words.
+
+        """
+        id2word = corpora.Dictionary(bigrams)
+        id2word.filter_extremes(no_below=5)
+        corpus = [id2word.doc2bow(text) for text in bigrams]
+        return id2word, corpus
+
+
+    def train_lda_model(corpus, id2word, num_topics=7, random_state=100):
+        """
+        Train an LDA model on the given corpus.
+
+        Args:
+            corpus (list): The preprocessed corpus as a list of bag-of-words.
+            id2word (gensim.corpora.Dictionary): The dictionary mapping of words to IDs.
+            num_topics (int): The number of topics to generate (default: 7).
+            random_state (int): The random state for reproducibility (default: 100).
+
+        Returns:
+            gensim.models.LdaModel: The trained LDA model.
+
+        """
+        model = LdaModel(corpus, id2word=id2word, num_topics=num_topics, random_state=random_state, eval_every=None)
+        return model
+
+
+    def save_model(model, output_path):
+        """
+        Save the trained LDA model to a file.
+
+        Args:
+            model (gensim.models.LdaModel): The trained LDA model.
+            output_path (str): The path to save the model.
+
+        """
+        model.save(output_path)
+
+        
+    if __name__ == "__main__":
+        # Define the file paths
+        corpus_model_file = 'corpus_model.csv'
+        model_output_file = os.path.join(os.getcwd(), 'lda_model')
+
+        # Load the corpus model
+        corpus_model = load_corpus_model(corpus_model_file)
+
+        # Preprocess the corpus
+        id2word, corpus = preprocess_corpus(corpus_model)
+
+        # Train the LDA model
+        num_topics = 7
+        random_state = 100
+        lda_model = train_lda_model(corpus, id2word, num_topics, random_state)
+
+        # Save the trained model to a file
+        save_model(lda_model, model_output_file)
+        
+        # Log parameters
+        mlflow.log_params({
+            "num_topics": num_topics,
+            "random_state": random_state
+        })
+        
+        # Log the model artifact
+        mlflow.log_artifact(model_output_file)
